f00ccf3399dac06a6ecb28de2867091f658ef089
mahabubr <mahabub.r753@gmail.com>
2025-04-19 01:08:36 +0600

    Update Qdrant client configuration and enhance notebook execution counts

--- app/qdrant/qdrant_client.py
+++ app/qdrant/qdrant_client.py
@@ -2,13 +2,11 @@ from qdrant_client import QdrantClient
 from app.config.dotenv import QDRANT_HOST, QDRANT_PORT
 
 
-qdrant = QdrantClient(
-    host=QDRANT_HOST,
-    port=int(QDRANT_PORT),
-)
+qdrant = QdrantClient(host=QDRANT_HOST, port=int(QDRANT_PORT), timeout=10000.0)
 
 
 def get_client():
     print(qdrant.get_collections())
+    print(qdrant.get_collection(collection_name="products").points_count)
 
     return qdrant
